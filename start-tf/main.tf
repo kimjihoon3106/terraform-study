6765cc4fa0c47e685a44b31f5079e6c22a823ad7
kimjihoon3106 <s24021@gsm.hs.kr>
2024-07-05 13:56:21 +0900

    intit

---
+++ start-tf/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "trusty-cacao-422307-j0"
+  credentials = file("/Users/kimjihoon/Downloads/trusty-cacao-422307-j0-d48becf971c7.json")
+  region      = "asia-northeast3"
+  zone        = "asia-northeast3-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
